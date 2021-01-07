7b921c8b866f989668b4dde3ed81a48e80a90674
sanggnguyenn <sang.nguyenngoc@gforces.auto>
2021-01-07 23:46:44 +0700

    optimize sass compiler

--- build/gulp/tasks/styles.js
+++ build/gulp/tasks/styles.js
@@ -1,4 +1,4 @@
-const { task, src, dest } = require('gulp');
+const { task, src, dest, lastRun } = require('gulp');
 const { extname } = require('path');
 
 const Fiber = require('fibers');
@@ -32,7 +32,10 @@ const postCssPlugins = [
 	cssnano(),
 ];
 
-const inputs = () => paths.pages('**', '*.s+(a|c)ss');
+const inputs = () => [
+	paths.app('**', '*.s+(a|c)ss'),
+	`!${paths.app('styles', '**', '*.s+(a|c)ss')}`,
+];
 
 const sourcemapsInit = () =>
 	isDev ? plugins.sourcemaps.init({ largeFile: true }) : pipe();
@@ -66,11 +69,71 @@ const concat = () =>
 		? plugins.concat(`${mainBundle}.css`)
 		: pipe();
 
+const filter = () => plugins.filter((file) => file.path.includes(paths._pages));
+
+const dependents = () => plugins.dependents();
+
+const dependentsConfig = {
+	'.scss': {
+		// The sequence of RegExps and/or functions to use when parsing
+		// dependency paths from a source file. Each RegExp must have the
+		// 'gm' modifier and at least one capture group. Each function must
+		// accept a string and return an array of captured strings. The
+		// strings captured by each RegExp or function will be passed
+		// to the next, thus iteratively reducing the file content to an
+		// array of dependency file paths.
+		parserSteps: [
+			// Please note:
+			// The parser steps shown here are only meant to illustrate
+			// the concept of a matching pipeline. The actual config used
+			// for scss files is pure RegExp and reliably supports the
+			// full syntax for import statements.
+
+			// Match the import statements and capture the text
+			// between "@import" and ";".
+			/^\s*@import\s+(.+?);/gm,
+
+			// Split the captured text on "," to get each path.
+			function (text) {
+				return text.split(',');
+			},
+
+			// Match the balanced quotes and capture only the file path.
+			/"([^"]+)"|'([^']+)'/gm,
+		],
+
+		// The file name prefixes to try when looking for dependency
+		// files, if the syntax does not require them to be specified in
+		// dependency statements. This could be e.g. '_', which is often
+		// used as a naming convention for mixin files.
+		prefixes: ['_'],
+
+		// The file name postfixes to try when looking for dependency
+		// files, if the syntax does not require them to be specified in
+		// dependency statements. This could be e.g. file name extensions.
+		postfixes: ['.sass', '.scss'],
+
+		// The additional base paths to try when looking for dependency
+		// files referenced using relative paths.
+		basePaths: [],
+	},
+};
+
 const compileStyles = () => {
 	const checkFiles = require(paths.core('checkFiles'));
 	checkFiles('styles');
-	return src(inputs(), { cwd: paths.app(dirs.styles) })
+	return src(inputs(), {
+		cwd: paths.app(dirs.styles),
+		since: lastRun('compile:styles'),
+	})
 		.pipe(pluginErrorHandle())
+		.pipe(
+			dependents(dependentsConfig, {
+				logDependents: true,
+				logDependencyMap: false,
+			})
+		)
+		.pipe(filter())
 		.pipe(sourcemapsInit())
 		.pipe(compileSass())
 		.pipe(postCss())
