d4a77f395b3f73e901134825a4e52567e202ca1e
sanggnguyenn <sang.nguyenngoc@gforces.auto>
2021-01-10 14:25:03 +0700

    add cleaning

--- build/gulp/tasks/watch.js
+++ build/gulp/tasks/watch.js
@@ -1,6 +1,33 @@
-const { watch, series, task } = require('gulp');
+const { watch, series, task, parallel } = require('gulp');
+const del = require('del');
+const { relative, resolve, sep, join } = require('path');
 
-const { isDev, paths, dirs, browserSync } = require('../../utils');
+const { isDev, paths, dirs, browserSync, taskTarget } = require('../../utils');
+
+const cleaningImages = async (file) => {
+	const config = {
+		force: true,
+		dot: true,
+	};
+	let filePathSrc;
+	let filePathBuild;
+
+	if (file.includes(paths._app)) {
+		let array = relative(paths._app, file)
+			.split(sep)
+			.slice(1)
+			.filter((f) => f !== dirs.assets);
+		let asset = [array[0]].concat(array.slice(1)).join(sep);
+
+		filePathBuild = resolve(taskTarget, dirs.static, asset);
+	} else if (file.includes(paths._assets)) {
+		filePathSrc = relative(paths._assets, file).split(sep).join(sep);
+		filePathBuild = resolve(taskTarget, filePathSrc);
+	}
+	console.log(` \u{1b}[32mDelete: ${filePathBuild}\u{1b}[0m`);
+
+	await del(filePathBuild, config);
+};
 
 const watchChanges = (done) => {
 	if (isDev) {
@@ -18,12 +45,14 @@ const watchChanges = (done) => {
 
 		// Images
 		watch(
-			[
-				paths.app('**', '*.{jpg,jpeg,gif,svg,png}'),
-				paths.assets('**', '*.{jpg,jpeg,gif,svg,png}'),
-			],
+			paths.assets('**', '*.{jpg,jpeg,gif,svg,png}'),
 			series('imagemin')
-		);
+		).on('unlink', (file) => cleaningImages(file));
+
+		watch(
+			paths.app('**', '*.{jpg,jpeg,gif,svg,png}'),
+			series('assets')
+		).on('unlink', (file) => cleaningImages(file));
 
 		// All other files
 		watch([
