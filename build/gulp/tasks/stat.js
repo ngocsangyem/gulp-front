268f79264c2e12ca9bb8274fc8fa296959800866
sanggnguyenn <sang.nguyenngoc@gforces.auto>
2021-04-23 13:41:27 +0700

    update dependencies

--- build/gulp/tasks/stat.js
+++ build/gulp/tasks/stat.js
@@ -1,15 +1,12 @@
 const { task, src } = require('gulp');
-const { writeFileSync } = require('fs');
 
 const { paths, plugins, colors } = require('../../utils');
-const { store } = require('../../utils/store');
 
 const sizeOpts = {
 	showFiles: true,
 };
 
 const stat = () => {
-	writeFileSync('store.json', JSON.stringify(store));
 	return src(paths.taskTarget('**', '*.*')).pipe(plugins.size(sizeOpts));
 };
 stat.description = `show sizing statistics for output files in current output directory, default being ${colors.cyan(
