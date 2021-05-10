35b24e04b4e2857bd2cae53d1858f102a00eafb5
sanggnguyenn <sang.nguyenngoc@gforces.auto>
2021-05-10 13:44:55 +0700

    add more scripts and eslint

---
+++ build/gulp/tasks/eslint.js
@@ -0,0 +1,31 @@
+const { task, src, dest } = require('gulp');
+const { paths, plugins, isDev, browserSync, dirs } = require('../../utils');
+
+const inputs = () => [
+	'**/*.js',
+	// Ignore all vendor folder files
+	'!**/tmp/**/*',
+	'!**/build/**/*',
+];
+
+const eslint = () =>
+	src(inputs(), { cwd: dirs.source })
+		.pipe(browserSync.reload({ stream: true, once: true }))
+		.pipe(
+			plugins.eslint({
+				useEslintrc: true,
+			})
+		)
+		.pipe(plugins.eslint.format())
+		.pipe(plugins.if(!browserSync.active, plugins.eslint.failAfterError()))
+		.on('error', function () {
+			if (!browserSync.active) {
+				process.exit(1);
+			}
+		});
+eslint.description = `check and format code by eslint rule`;
+eslint.flags = {
+	'--eslint': `check and format code by eslint rule`,
+};
+
+task(eslint);
